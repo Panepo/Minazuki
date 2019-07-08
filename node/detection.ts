5fc66ac61db0652d0fd890ff2e9a5416f413575a
Panepo <whisper.ko@gmail.com>
2019-07-08 15:08:16 +0800

    add src
    add node

---
+++ node/detection.ts
@@ -0,0 +1,29 @@
+import * as faceapi from 'face-api.js'
+
+import {
+  canvas,
+  faceDetectionNet,
+  faceDetectionOptions,
+  saveFile,
+  modelLink
+} from './commons'
+
+async function run() {
+  await faceDetectionNet.loadFromDisk(modelLink)
+  const tstart = process.hrtime()
+
+  const img = await canvas.loadImage('./h1.jpg')
+  const detections = await faceapi.detectAllFaces(img, faceDetectionOptions)
+
+  const out = faceapi.createCanvasFromMedia(img) as any
+  faceapi.draw.drawDetections(out, detections)
+
+  const tend = process.hrtime(tstart)
+  console.info('[INFO] total process time: %dms', tend[1] / 1000000)
+
+  const fileName = 'detection_' + new Date().getTime().toString() + '.jpg'
+  saveFile(fileName, out.toBuffer('image/jpeg'))
+  console.info('[INFO] saved results to out/' + fileName)
+}
+
+run()
---
+++ node/landmark.ts
@@ -0,0 +1,33 @@
+import * as faceapi from 'face-api.js'
+
+import {
+  canvas,
+  faceDetectionNet,
+  faceDetectionOptions,
+  saveFile,
+  modelLink
+} from './commons'
+
+async function run() {
+  await faceDetectionNet.loadFromDisk(modelLink)
+  await faceapi.nets.faceLandmark68Net.loadFromDisk(modelLink)
+  const tstart = process.hrtime()
+
+  const img = await canvas.loadImage('./h1.jpg')
+  const results = await faceapi
+    .detectAllFaces(img, faceDetectionOptions)
+    .withFaceLandmarks()
+
+  const out = faceapi.createCanvasFromMedia(img) as any
+  faceapi.draw.drawDetections(out, results.map(res => res.detection))
+  faceapi.draw.drawFaceLandmarks(out, results.map(res => res.landmarks))
+
+  const tend = process.hrtime(tstart)
+  console.info('[INFO] total process time: %dms', tend[1] / 1000000)
+
+  const fileName = 'landmark_' + new Date().getTime().toString() + '.jpg'
+  saveFile(fileName, out.toBuffer('image/jpeg'))
+  console.info('[INFO] saved results to out/' + fileName)
+}
+
+run()

22404ee28c50e6f68ef55c16aa5bbd74d0d71084
Panepo <whisper.ko@gmail.com>
2019-08-12 10:22:21 +0800

    update node
    add argument for node

--- node/recognition.ts
+++ node/recognition.ts
@@ -9,7 +9,15 @@ import {
 } from './utils'
 import { createFaceMatcher } from './utils/faceMatch'
 
-async function run() {
+// process argument
+const input = process.argv[2]
+
+if (!input) {
+  console.error("[ERROR] the path of input image is required")
+  process.exit(0)
+}
+
+async function run(input: string) {
   // load face recognition model
   await faceDetectionNet.loadFromDisk(modelLink)
   await faceapi.nets.faceLandmark68Net.loadFromDisk(modelLink)
@@ -25,7 +33,7 @@ async function run() {
   const faceMatcher = await createFaceMatcher(faces)
 
   // open an image file
-  const img = await canvas.loadImage('../images/pos1.jpg')
+  const img = await canvas.loadImage(input)
   // find all the faces and face encodings in the current image
   const results = await faceapi
     .detectAllFaces(img, faceDetectionOptions)
@@ -41,6 +49,8 @@ async function run() {
     const outQuery = faceapi.createCanvasFromMedia(img)
     queryDrawBoxes.forEach(drawBox => drawBox.draw(outQuery))
 
+    faceapi.draw.drawFaceLandmarks(outQuery, results.map(res => res.landmarks))
+
     // save results
     saveFile('recognition', (outQuery as any).toBuffer('image/jpeg'))
   } else {
@@ -51,4 +61,4 @@ async function run() {
   console.info('[INFO] total process time: %dms', tend[1] / 1000000)
 }
 
-run()
+run(input)
